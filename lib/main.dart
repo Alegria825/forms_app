cd3c2c3f1eddfa52340d70eb0e9984fe6c785ccc
Alegria <ealejandropa2005@gmail.com>
2026-02-27 00:42:07 -0600

    first commit forms app

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:forms_app/Config/router/app_router.dart';
+import 'package:forms_app/Config/theme/app_theme.dart';
+
+void main() {
+  runApp(const MainApp());
+
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router(
+      routerConfig: appRouter,
+      debugShowCheckedModeBanner: false,
+      theme: AppTheme().getTheme(),
+    );
+  }
+}
