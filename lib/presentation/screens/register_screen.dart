7221a1ae39d82b3058b71d6d9b54af8abc6f0914
Alegria <ealejandropa2005@gmail.com>
2026-03-17 13:55:02 -0600

    Traditional form

--- lib/presentation/screens/register_screen.dart
+++ lib/presentation/screens/register_screen.dart
@@ -7,14 +7,10 @@ class RegisterScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Nuevo usuario"),
-      ),
+      appBar: AppBar(title: Text("Nuevo usuario")),
       body: _RegisterView(),
     );
   }
-
-  
 }
 
 class _RegisterView extends StatelessWidget {
@@ -29,44 +25,104 @@ class _RegisterView extends StatelessWidget {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.end,
             children: [
-
               const FlutterLogo(size: 100),
 
               _RegisterForm(),
 
-
-              SizedBox(height: 40)
+              SizedBox(height: 40),
             ],
           ),
         ),
       ),
     );
-  } 
+  }
 }
 
-class _RegisterForm extends StatelessWidget {
+class _RegisterForm extends StatefulWidget {
   const _RegisterForm();
+
+  @override
+  State<_RegisterForm> createState() => _RegisterFormState();
+}
+
+class _RegisterFormState extends State<_RegisterForm> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  String userName = '';
+  String lastName = '';
+  String enrollment = '';
+  String email = '';
+  String password = '';
 
   @override
   Widget build(BuildContext context) {
     return Form(
+      key: _formKey,
       child: Column(
         children: [
+          CustomTextFormField(
+            label: 'Nombre',
+            onChanged: (value) => userName = value,
+            validator: (value) {
+              if (value == null || value.isEmpty) return 'Campo requerido';
+              if (value.trim().isEmpty) return 'Campo requerido';
+              return null;
+            },
+          ),
+          SizedBox(height: 20),
 
-            CustomTextFormField(), 
-            SizedBox(height: 20),
-            
-            CustomTextFormField(),
-            SizedBox(height: 40),
-            
+          CustomTextFormField(
+            label: 'Apellidos',
+            onChanged: (value) => lastName = value,
+          ),
+          SizedBox(height: 20),
 
+          CustomTextFormField(
+            label: 'Matricula',
+            onChanged: (value) => enrollment = value,
+          ),
+          SizedBox(height: 20),
 
-            FilledButton.tonalIcon(
-              onPressed: () {}, 
-              icon: Icon(Icons.save),
-              label: Text('Crear usuario'),
-              ),
+          CustomTextFormField(
+            label: 'correo electronico',
+            onChanged: (value) => email = value,
+            validator: (value) {
+              if (value == null || value.isEmpty) return 'Campo requerido';
+              if (value.trim().isEmpty) return 'Campo requerido';
+              final emailRegExp = RegExp(r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$');
+
+              if (!emailRegExp.hasMatch(value))
+                return 'No tiene formato de correo';
+
+              return null;
+            },
+          ),
+          SizedBox(height: 20),
+
+          CustomTextFormField(
+            label: 'contraseña',
+            obscureText: true,
+            onChanged: (value) => password = value,
+            validator: (value) {
+              if (value == null || value.isEmpty) return 'Campo requerido';
+              if (value.trim().isEmpty) return 'Campo requerido';
+              if (value.length < 6) return 'Debe de contener mas de 6 caracteres';
+              return null;
+            },
+          ),
+
+          FilledButton.tonalIcon(
+            onPressed: () {
+              final isValid = _formKey.currentState!.validate();
+              if (!isValid) return;
+
+              print('$userName, $lastName, $enrollment, $email, $password');
+            },
+            icon: Icon(Icons.save),
+            label: Text('Crear usuario'),
+          ),
         ],
-      ));
+      ),
+    );
   }
 }
--- lib/presentation/widgets/inputs/custom_text_form_field.dart
+++ lib/presentation/widgets/inputs/custom_text_form_field.dart
@@ -1,7 +1,25 @@
 import 'package:flutter/material.dart';
 
 class CustomTextFormField extends StatelessWidget {
-  const CustomTextFormField({super.key});
+
+  final String? label; 
+  final String? hint;
+  final String? errorMessage;
+  final Function(String)? onChanged;
+  final String? Function(String?)? validator;
+  final bool obscureText;
+
+  
+  const CustomTextFormField({
+    super.key, 
+    this.label, 
+    this.hint, 
+    this.errorMessage, 
+    this.onChanged, 
+    this.validator, 
+    this.obscureText = false,
+    });
+
 
   @override
   Widget build(BuildContext context) {
@@ -15,21 +33,21 @@ class CustomTextFormField extends StatelessWidget {
 
 
     return TextFormField(
-      onChanged: (value) {
-        print('Value: $value');
-      },
-
+      onChanged: onChanged,
       //*EJEMPLOS DE VALIDADORES
-      validator: (value) {
-        if (value == null) return 'campo requerido';
-        if (value.isEmpty) return 'campo requerido';
-        if (value.trim().isEmpty) return 'campo requerido';
-
-        return null;
-      },
+      validator: validator,
+      obscureText: obscureText,      
       decoration: InputDecoration(
         enabledBorder: border,
-        focusedBorder: border.copyWith(borderSide: BorderSide(color: colors.primary))
+        focusedBorder: border.copyWith(borderSide: BorderSide(color: colors.primary)),
+        errorBorder: border.copyWith(borderSide: BorderSide(color: Colors.red.shade800)),
+        focusedErrorBorder: border.copyWith(borderSide: BorderSide(color: Colors.red.shade800)),
+
+        isDense: true,
+        label: label != null ? Text(label!) : null,
+        hintText: hint,
+        errorText: errorMessage,
+        focusColor: colors.primary,
       ),
     );
   }
